91cb098f07e89f8a5daf402056d05973508edbe8
Tomasz Zablocki <tomaszzablocki@Tomaszs-MacBook-Pro.local>
2019-07-17 20:32:51 +0200

    Initialized project

---
+++ src/main/java/com/lowlatency/Main.java
@@ -0,0 +1,16 @@
+package com.lowlatency;
+
+import com.speedment.enterprise.application.InMemoryBundle;
+import com.speedment.enterprise.datastore.runtime.DataStoreComponent;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    
+    public static void main(final String... args) {
+        /*
+        SpringApplication.run(Main.class, args);
+        */
+    }
+}
